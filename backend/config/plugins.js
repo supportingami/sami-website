dbad4cf256a85393b8ce9bfc1ea0d13d84e82fe7
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-13 19:38:06 -0700

    feat: add graphql extension

---
+++ backend/config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  "import-export-entries": {
+    enabled: true,
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
