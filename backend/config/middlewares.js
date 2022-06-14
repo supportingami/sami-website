7969ea9caf3a72c6a35d7df1c9a35db87c1d3506
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-13 19:36:02 -0700

    feat: add sqlite dev config

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,24 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  // Required for https://github.com/Baboo7/strapi-plugin-import-export-entries
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "script-src": ["'self'", "cdn.jsdelivr.net", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
