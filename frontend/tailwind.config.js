2842337383cd68cfa22dbfa6c798b314b2e13cb3
Chris <chris.m.clarke@live.co.uk>
2022-08-30 10:27:24 +0100

    feat: add sami base theme

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      // SAMI themes extending default daisy themes (can be further adapted)
+      // https://daisyui.com/docs/themes/
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#53b4f4",
+          "primary-focus": "mediumblue",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#89a9c5",
+        },
+      },
+
+      // Additional daisy themes can be included below if wanted
+      // https://daisyui.com/docs/themes/
+      // "cupcake",
+      // "bumblebee",
+    ],
+  },
+};
