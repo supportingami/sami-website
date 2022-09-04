d3c544904da8afead178952ea1dcd8a684ce7c47
FaithDaka <faithbusigu8@gmail.com>
2022-09-04 20:35:53 +0300

    feat: members

--- frontend/components/pages/members/list-item.tsx
+++ frontend/components/pages/members/list-item.tsx
@@ -1,14 +1,23 @@
-import { Box } from "@chakra-ui/core";
+import { getStrapiMedia } from "lib/media";
+import Image from "next/image";
 import React from "react";
 import { IMember } from "types/member";
 
 export const MembersListItemComponent: React.FC<{
   member: IMember;
 }> = ({ member }) => (
-  <Box>
+  <div style={{ border: "0.8px solid #ddd", margin: "0 3px" }}>
     <div>
-      <div>Name{member.Name}</div>
-      <div>Email {member.Email}</div>
+      {member.Photo && <Image src={getStrapiMedia(member.Photo)} alt={`${member.Name}`} height="300px" width="300px" />}
     </div>
-  </Box>
+    {member.Organisation === "SAMI" ? (
+      <div style={{ fontSize: "14px", backgroundColor: "#4a8eff", color: "white", padding: "3px 5px" }}>
+        {member.Name}
+      </div>
+    ) : (
+      <div style={{ fontSize: "14px", backgroundColor: "#ff5062", color: "white", padding: "3px 5px" }}>
+        {member.Name}
+      </div>
+    )}
+  </div>
 );
