0e53e281600e2a5143a31f9d92c209d2d8c105c5
chrismclarke <chris.m.clarke@live.co.uk>
2022-10-09 11:04:57 -0700

    feat: add home content and component schema

---
+++ backend/src/api/home-content/controllers/home-content.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ *  home-content controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::home-content.home-content");
---
+++ backend/src/api/home-content/services/home-content.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * home-content service.
+ */
+
+const { createCoreService } = require("@strapi/strapi").factories;
+
+module.exports = createCoreService("api::home-content.home-content");
