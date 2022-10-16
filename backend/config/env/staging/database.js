6e406a7e745619d8c06ec2cda79b5982dab2a633
chrismclarke <chris.m.clarke@live.co.uk>
2022-10-15 21:42:38 -0700

    feat: add support for staging backend

---
+++ backend/config/env/staging/database.js
@@ -0,0 +1,23 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+      },
+      pool: {
+        min: 2,
+        max: 5,
+      },
+      debug: false,
+    },
+  };
+};
