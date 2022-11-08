dc3d0f63d30daf397f2eec54284ce7f050029858
chrismclarke <chris.m.clarke@live.co.uk>
2022-11-07 23:01:42 -0800

    chore: lint fixes

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -44,6 +44,7 @@ const Navbar = () => {
   ];
 
   // TODO - might need arrow for nested nav, TBC
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const ArrowRight = () => (
     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
       <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
