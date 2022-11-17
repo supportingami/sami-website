fad84e50835423d4ec0e48f08f7c42e8e13a30c7
chrismclarke <chris.m.clarke@live.co.uk>
2022-11-17 10:04:30 -0800

    style: update theming and add home placeholders

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -8,6 +8,7 @@ import { HeroImageComponent } from "components/common/heroImage";
 import { MissionStatementComponent } from "components/pages/home/missionStatement";
 import PageSection from "components/layout/pageSection";
 import { ProjectSummaryComponent } from "components/pages/home/ProjectSummary";
+import { GetInvolvedComponent } from "components/pages/home/getInvolved";
 
 export const getServerSideProps = async ({}: GetServerSidePropsContext) => {
   const res = await serverQuery<HomeContentQuery>(HomeContentDocument);
@@ -23,17 +24,43 @@ const HomePage = ({ content }: InferGetServerSidePropsType<typeof getServerSideP
       {content.HeroImages?.[0] && <HeroImageComponent heroImage={content.HeroImages[0] as any} />}
 
       {content.MissionStatement && (
-        <PageSection className="pt-24">
+        <PageSection className="py-16">
           <MissionStatementComponent {...(content.MissionStatement as any)} />
         </PageSection>
       )}
 
-      <PageSection fullwidth className="bg-primary primary-content text-white">
+      <PageSection fullwidth className="bg-primary primary-content text-white py-16">
         (TODO - stats)
       </PageSection>
-      <PageSection fullwidth>
+      <PageSection className="py-16">
         {content.ProjectSummary && <ProjectSummaryComponent {...(content.ProjectSummary as any)} />}
       </PageSection>
+      <PageSection fullwidth className="bg-base-200 py-16">
+        {content.GetInvolved && <GetInvolvedComponent {...(content.GetInvolved as any)} />}
+      </PageSection>
+      <PageSection fullwidth className="bg-primary-focus text-white py-0">
+        <h3 className="text-center">Latest News</h3>
+      </PageSection>
+      <PageSection className="py-16">(TODO - blog posts)</PageSection>
+      <PageSection fullwidth className="bg-accent text-white py-0">
+        <h3 className="text-center">You can help us make a difference</h3>
+      </PageSection>
+      <PageSection fullwidth className="bg-base-200 py-16">
+        (TODO - donate buttons)
+      </PageSection>
+      <PageSection fullwidth className="bg-primary text-white text-center py-16">
+        <h3>Sign up to get latest updates</h3>
+        <p>
+          Be the first to hear about new projects and receive occasional updates about SAMI
+          <br></br>
+          <br></br>
+          (TODO - mailing list signup)
+        </p>
+      </PageSection>
+      <PageSection fullwidth className="bg-base-300 py-16">
+        (TODO - sitemap)
+      </PageSection>
+      <PageSection className="py-4">(TODO - footer)</PageSection>
     </>
   </>
 );
