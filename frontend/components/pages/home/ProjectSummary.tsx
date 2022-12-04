daafed91940ec77b15d889771ad90cece00689f7
chrismclarke <chris.m.clarke@live.co.uk>
2022-12-03 17:49:52 -0800

    refactor: home column layouts

--- frontend/components/pages/home/ProjectSummary.tsx
+++ frontend/components/pages/home/ProjectSummary.tsx
@@ -4,6 +4,7 @@ import { ActionButtonsComponent } from "components/common/actionButtons";
 import { getStrapiMedia } from "lib/media";
 import { ProjectSummaryItem } from "../projects/projectSummaryItem";
 import type { IProject } from "types/project";
+import { ImageHeadingContentLayout } from "components/layout/columns";
 
 export const ProjectSummaryComponent: React.FC<ComponentHomeProjectSummary & { Projects: IProject[] }> = ({
   Title,
@@ -11,22 +12,16 @@ export const ProjectSummaryComponent: React.FC<ComponentHomeProjectSummary & { P
   Image,
   Projects,
 }) => (
-  <>
-    <div data-testid="projectSummary" className="flex align-center gap-32">
-      <div className="flex-1 relative hidden lg:block rounded-md overflow-hidden">
-        <ProjectSummaryImage {...Image} />
-      </div>
-      <div className="flex-1">
-        <h2 className="subtitle">{Title}</h2>
-        {/* Mobile Image under text (dektop alongside) */}
-        <div className="relative mb-6 lg:hidden h-[300px] mx-auto max-w-md rounded-md">
-          <ProjectSummaryImage {...Image} />
-        </div>
+  <ImageHeadingContentLayout
+    Heading={<h2 className="subtitle">{Title}</h2>}
+    Image={<ProjectSummaryImage {...Image} />}
+    Content={
+      <>
         {Projects && Projects.map((project) => <ProjectSummaryItem key={project.id} {...project} />)}
         <ActionButtonsComponent actionButtons={ActionButtons} className="mt-8" />
-      </div>
-    </div>
-  </>
+      </>
+    }
+  />
 );
 
 const ProjectSummaryImage = (ImageData: Partial<UploadFileEntityResponse>) =>
