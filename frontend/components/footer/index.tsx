d81da14bb19604072d5fc23e7c7b128d2e624867
chrismclarke <chris.m.clarke@live.co.uk>
2023-02-06 12:23:34 -0800

    chore: code tidying

--- frontend/components/footer/index.tsx
+++ frontend/components/footer/index.tsx
@@ -3,11 +3,11 @@ import Sitemap from "./sitemap";
 import Newsletter from "./newsletter";
 
 export const Footer = () => (
-  <>
+  <footer>
     <Newsletter />
     <Sitemap />
     <SmallPartners />
-  </>
+  </footer>
 );
 
 export default Footer;
