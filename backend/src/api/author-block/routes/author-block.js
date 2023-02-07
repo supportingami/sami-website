bedbdb0c24b7165702014b005801f4318b5bd05c
chrismclarke <chris.m.clarke@live.co.uk>
2023-02-06 22:13:38 -0800

    chore: update schema

---
+++ backend/src/api/author-block/routes/author-block.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * author-block router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::author-block.author-block');
---
+++ backend/src/api/author-block/services/author-block.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * author-block service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::author-block.author-block');
