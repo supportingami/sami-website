804103e0e27ef67937b93382463277f41cd34e12
chrismclarke <chris.m.clarke@live.co.uk>
2024-03-09 13:40:45 -0800

    chore: code tidying

--- scripts/src/commands/strapi/export/storage.ts
+++ scripts/src/commands/strapi/export/storage.ts
@@ -1,23 +1,14 @@
-import { resolve } from "path";
 import { logError } from "../../../utils";
-import { PATHS } from "../../../paths";
-import { replicateDir } from "../../../utils/file.utils";
 
 export class StorageExport {
   public async run(gcsBucketName?: string) {
     if (gcsBucketName) {
       return this.exportGoogleStorage();
     }
-    return this.exportLocalStorage();
+    // local strapi already writes storage to data dir
   }
   private exportGoogleStorage() {
     // TODO
     logError({ msg1: "Google storage export not currently implemented" });
-  }
-
-  private exportLocalStorage() {
-    const source = resolve(PATHS.backendDir, "public", "uploads");
-    const target = resolve(PATHS.dataDir, "uploads");
-    replicateDir(source, target);
   }
 }
