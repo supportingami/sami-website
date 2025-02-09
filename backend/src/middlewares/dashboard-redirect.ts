e55c1288ec04375c7400475408a31d3ec074e942
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-08 20:58:16 -0800

    feat: strapi 5 update

--- backend/src/middlewares/dashboard-redirect.ts
+++ backend/src/middlewares/dashboard-redirect.ts
@@ -1,10 +1,10 @@
-import { Strapi } from "@strapi/strapi";
+import type { Core } from "@strapi/types";
 
 /**
  * Redirect / to dashboard admin page (e.g. /admin or /dashboard)
  * https://stackoverflow.com/a/71981103/5693245
  */
-export default (config: any, { strapi }: { strapi: Strapi }) => {
+export default (config: any, { strapi }: { strapi: Core.Strapi }) => {
   return async (ctx, next) => {
     if (ctx.path === "/") {
       ctx.redirect(strapi.config.get("server.admin.url", "/admin"));
